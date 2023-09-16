91197a74123c069bc6ac0d3ca151e7ad18ac5000
Shubham Panwar <shubham043panwar@gmail.com>
2023-09-16 14:39:20 +0530

    pascal triangle

---
+++ pascal_triangle2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int facNcR(int n,int c){
+         int res = 1;
+         for(int i=0; i<c; i++){
+            res = res*(n-i);
+            res = res/(i+1);
+
+         }
+         return res;
+    
+}
+
+int main(){
+
+// printing the nth row of the pascal's triangle
+int n,i;
+cin>>n;
+for(i=1; i<=n; i++){
+        cout<<facNcR(n-1,i-1)<<" ";
+}
+
+
+    return 0;
+}
